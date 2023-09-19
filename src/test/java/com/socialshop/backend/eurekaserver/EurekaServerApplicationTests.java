d83da78b108c5b9f06eb9aa11fcf1dcae4c458b1
Hung Nguyen <nxhung16102001@gmail.com>
2023-09-19 11:28:21 +0700

    [DEV] test case

--- src/test/java/com/socialshop/backend/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/socialshop/backend/eurekaserver/EurekaServerApplicationTests.java
@@ -2,12 +2,29 @@ package com.socialshop.backend.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void eurekaServerIsRunning() {
+		RestTemplate restTemplate = new RestTemplate();
+		String statusUrl = "http://localhost:" + port + "/actuator/health";
+		String response = restTemplate.getForObject(statusUrl, String.class);
+		assertEquals("{\"status\":\"UP\"}", response);
+    }
 
 }
